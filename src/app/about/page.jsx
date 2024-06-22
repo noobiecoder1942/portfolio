3095ec04edd2c821f108006f8f4235c08fccdb1a
Abhirag Nagpure <abhiragnagpure@gmail.com>
2024-06-22 19:54:59 -0400

    Add About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import NavBar from '@/components/NavBar';
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <main className="bg-light dark:bg-dark w-full min-h-screen">
+        <NavBar />
+      </main>
+    </>
+  );
+};
+
+export default About;
