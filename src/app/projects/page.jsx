b843be4da1cbd392abe31663db6228ec7d563dbf
Abhirag Nagpure <abhiragnagpure@gmail.com>
2024-06-24 17:13:15 -0400

    Added projects page

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '@/components/NavBar';
+
+const Projects = () => {
+    return (
+        <>
+          <main className="bg-light dark:bg-dark w-full min-h-screen">
+            <NavBar />
+          </main>
+        </>
+      );
+}
+
+export default Projects;
